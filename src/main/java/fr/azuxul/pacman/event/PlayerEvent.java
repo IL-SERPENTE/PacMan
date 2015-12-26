ad1755123fc79471383a7a639d6c6d92057514e2
Azuxul <contact@azuxul.fr>
2015-12-26 23:07:07 +0100

    Fix NulPointerException

--- src/main/java/fr/azuxul/pacman/event/PlayerEvent.java
+++ src/main/java/fr/azuxul/pacman/event/PlayerEvent.java
@@ -174,7 +174,7 @@ public class PlayerEvent implements Listener {
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
 
-        if (!event.getItem().getType().equals(Material.WRITTEN_BOOK) && !(event.getAction().equals(Action.RIGHT_CLICK_BLOCK) || event.getAction().equals(Action.RIGHT_CLICK_AIR)))
+        if (event.getItem() == null || (!event.getItem().getType().equals(Material.WRITTEN_BOOK) && !(event.getAction().equals(Action.RIGHT_CLICK_BLOCK) || event.getAction().equals(Action.RIGHT_CLICK_AIR))))
             event.setCancelled(true); // Cancel player interaction
     }
 
