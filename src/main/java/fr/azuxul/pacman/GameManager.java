73797ce8fbe3de400d601c7b70f7a4a3ac8159da
Azuxul <contact@azuxul.fr>
2016-03-11 18:47:44 +0100

    Fix winner reward

--- src/main/java/fr/azuxul/pacman/GameManager.java
+++ src/main/java/fr/azuxul/pacman/GameManager.java
@@ -249,7 +249,7 @@ public class GameManager extends Game<PlayerPacMan> {
             }
 
             if (winners.contains(playerPacMan)) {
-                if (winners.indexOf(playerPacMan) == 0) {
+                if (winners.indexOf(playerPacMan) == 3) {
                     playerPacMan.addCoins(30, "Partie gagnée");
                     playerPacMan.addStars(2, "Partie gagnée");
                 } else {
