5752898dbc5eaff78e0db9d2336cd89ba7d21ff4
Azuxul <contact@azuxul.fr>
2016-03-12 23:57:51 +0100

    Add rank on scoreboard

--- src/main/java/fr/azuxul/pacman/scoreboard/ScoreboardPacMan.java
+++ src/main/java/fr/azuxul/pacman/scoreboard/ScoreboardPacMan.java
@@ -133,7 +133,7 @@ public class ScoreboardPacMan {
 
             try {
                 line++;
-                objectiveSign.setLine(line, ChatColor.GRAY + playerPacManDisplay.getOfflinePlayer().getName() + ChatColor.GRAY + " : " + ChatColor.GREEN + playerPacManDisplay.getGomme());
+                objectiveSign.setLine(line, ChatColor.GRAY + playerPacManDisplay.getPlayerIfOnline().getDisplayName() + ChatColor.GRAY + " : " + ChatColor.GREEN + playerPacManDisplay.getGomme());
             } catch (NullPointerException e) {
                 gameManager.getServer().getLogger().warning(String.valueOf(e));
             }
